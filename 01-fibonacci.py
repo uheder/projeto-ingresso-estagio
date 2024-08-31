3958ce90f1502340c1f90eb818169c354435c253
Uheder Macedo <dev.usmacedo@outlook.com>
2024-08-31 12:44:33 -0300

    fix: ajustado para casos 0 e 1

--- 01-fibonacci.py
+++ 01-fibonacci.py
@@ -2,14 +2,16 @@ def is_fibonacci(n: int)-> bool:
     if n < 0:
         return False
     lista = [0, 1]
-
+    
+    if n == 0 or n == 1:
+        return "É um número da sequência Fibonacci"
     i = 0
     while lista[i + 1] < n:
         lista.append(lista[i] + lista[i + 1])
         if n in lista:
-            return True
+            return "É um número da sequência Fibonacci"
         i += 1
-    return False
+    return "Não é um número da sequência Fibonacci"
 
 
 print(is_fibonacci(int(input('Insira o número a ser verificado: '))))
